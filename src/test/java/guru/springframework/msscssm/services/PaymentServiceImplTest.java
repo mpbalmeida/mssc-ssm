cd0d5b002eddfc53e0eaeef29d6b5d8c3a59d42b
Marcos Almeida <marcos@marcosalmeida.dev>
2021-07-31 17:46:08 +0200

    feat: state machine actions

--- src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -1,14 +1,22 @@
 package guru.springframework.msscssm.services;
 
 import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
 import guru.springframework.msscssm.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import static guru.springframework.msscssm.domain.PaymentState.PRE_AUTH;
+import static guru.springframework.msscssm.domain.PaymentState.PRE_AUTH_ERROR;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PaymentServiceImplTest {
@@ -32,11 +40,8 @@ class PaymentServiceImplTest {
 
         Payment savedPayment = service.newPayment(this.payment);
 
-        service.preAuth(savedPayment.getId());
+        StateMachine<PaymentState, PaymentEvent> stateMachine = service.preAuth(savedPayment.getId());
 
-        Payment preAuthPayment = paymentRepository.getById(savedPayment.getId());
-
-        System.out.println(preAuthPayment);
-
+        assertTrue(List.of(PRE_AUTH, PRE_AUTH_ERROR).contains(stateMachine.getState().getId()));
     }
 }
